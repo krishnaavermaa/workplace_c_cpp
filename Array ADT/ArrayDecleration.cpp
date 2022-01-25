cb71eb7a3adcbe9f97f316637c8e0289f77e734c
krishna <krishna.1923en1078@kiet.edu>
2022-01-25 18:17:56 +0530

    ArrayDecleration program completed

--- Array ADT/ArrayDecleration.cpp
+++ Array ADT/ArrayDecleration.cpp
@@ -37,7 +37,6 @@ public:
 	void Append(int x);
 	void Insert(int index, int x);
 	void Delete(int index);
-	void swap(int* x,int* y);
 	int LinearSearch(int key);
 	int BinarySearch(int key);
 	int Get(int index);
@@ -47,14 +46,16 @@ public:
 	int Sum();
 	float Avg();
 	void Reverse();
-	void Reverse2();
+	void RevDigits();
+	void BubbleSort();
+	void SelectionSort();
 	void InsertSort(int x);
 	int isSorted();
 	void Rearrange();
-	Array Merge(Array arr2);
-	Array* Union(Array arr2);
-	Array* Diff(Array arr2);
-	Array* Inter(Array arr2);
+	Array Merge(Array arr2);//works if both i/p arrays are similarly sorted
+	Array Union(Array arr2);//works if both i/p arrays are similarly sorted
+	Array Diff(Array arr2);//works if both i/p arrays have same length 
+	Array Inter(Array arr2);//works if both i/p arrays are similarly sorted
 };
 
 int Array::Get(int index)
@@ -68,6 +69,58 @@ void Array::Set(int index, int x)
 {
 	if(index>=0 && index<length)
 		A[index]=x;
+}
+
+void Array::swap(int* x,int* y)
+{
+	int temp=*x;
+	*x=*y;
+	*y=temp;
+}
+
+int Array::LinearSearch(int key)
+{
+	for(int i=0;i<length;i++)
+	{
+		if(A[i]==key) return i;
+	}
+	return -1;
+}
+
+int Array::BinarySearch(int key)
+{
+	int s=0,e=length-1,m,sort=isSorted();
+	while(s<=e)
+	{
+		m=(s+e)/2;
+		if(A[m]==key) return m;
+		if(sort==1)//asc
+		{
+			if(A[m]>key) e=m-1;
+			else s=m+1;
+		}
+		if(sort==-1)//des
+		{
+			if(A[m]>key) s=m+1;
+			else e=m-1;
+		}
+	}
+	return -1;
+}
+
+int Array::isSorted()
+{
+	int asc=0,des=0;
+	for(int i=1;i<length;i++)
+	{
+		if(A[i-1]-A[i]>0)
+			des++;
+		else if(A[i-1]-A[i]<0)
+			asc++;
+	}
+	if(asc==length-1) return 1;//where 1 represents ascendingly sorted array
+	if(des==length-1) return -1;//where -1 represents descendingly sorted array
+	return 0;//means unsorted array
 }
 
 void Array::Display()
@@ -107,29 +160,232 @@ void Array::Delete(int index)
 	}
 }
 
-void Array::swap(int* x,int* y)
+int Array::Max()
 {
-	int temp=*x;
-	*x=*y;
-	*y=temp;
+	int max=A[0];
+	for(int i=0;i<length;i++)
+	{
+		if(max<A[i])
+			max=A[i];
+	}
+	return max;
 }
 
+int Array::Min()
+{
+	int min=A[0];
+	for(int i=0;i<length;i++)
+	{
+		if(min>A[i])
+			min=A[i];
+	}
+	return min;
+}
+
+float Array::Avg()
+{
+	float avg=0;
+	for(int i=0;i<length;i++)
+		avg=avg+A[i];
+	avg=avg/(float)length;
+	return avg;
+}
+
+void Array::Reverse()
+{
+	for(int i=0,j=length-1;i<j;i++,j--)
+	{
+		/*int temp=A[i];
+		A[i]=A[j];
+		A[j]=temp;*/
+		swap(&A[i],&A[j]);
+	}
+}
+
+void Array::RevDigits()
+{
+	int n,sum,l;
+	for(int i=0;i<length-1;i++)
+	{
+		n=A[i],sum=0,l=length;
+		while(n!=0)
+		{
+			sum=sum+(n%10)*pow(10,--l);
+			n=n/10;
+		}
+		A[i]=sum;
+	}
+}
+
+void Array::BubbleSort()//for ascending sort
+{
+	for(int i=length-1;i>0;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if(A[j-1]>A[j]) swap(&A[i],&A[j]);
+		}
+	}
+}
+
+void Array::SelectionSort()//for descending sort
+{
+	for(int i=0;i<length-1;i++)
+	{
+		for(int j=i;j<length-1;j++)
+		{
+			if(A[j]>A[i]) swap(&A[j], &A[i]);
+		}
+	}
+}
+
+void Array::InsertSort(int x)
+{
+	int i=length-1;
+	if(length<size)
+	{
+		if(isSorted()==1)
+		{
+			while(A[i]>x) Insert(i+1,A[i--]);
+			A[--i]=x;
+		}
+		if(isSorted()==-1)
+		{
+			while(A[i]<x) Insert(i+1,A[i--]);
+			A[--i]=x;
+		}
+	}
+}
+
+void Array::Rearrange()
+{
+	int i=0,j=length-1;
+	while(i<j)
+	{
+		while(A[i]<0) i++;
+		while(A[j]>=0) j++;
+		if(i<j) swap(&A[i], &A[j]);
+	}
+}
+
+//this fn works ONLY if input arrays are sorted either BOTH ascendingly 
+//or BOTH descendingly
 Array Array::Merge(Array arr2)
 {
 	int i,j,k;
 	i=j=k=0;
 	Array arr3(length+arr2.length);
-	while(i<length && j<arr2.length)
+	if(arr2.isSorted()!=isSorted()) return arr3;
+	int sorted=arr2.isSorted();
+	if(sorted==1)//ascending
 	{
-		if(A[i]<=arr2.Get(j))
-			arr3.Append(A[i++]);
-		else arr3.Append(arr2.Get(j++));
-		k++;
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]<=arr2.Get(j))
+				arr3.Append(A[i++]);
+			else arr3.Append(arr2.Get(j++));
+			k++;
+		}
+	}
+	else if(sorted==-1)//descending
+	{
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]>=arr2.Get(j))
+				arr3.Append(A[i++]);
+			else arr3.Append(arr2.Get(j++));
+			k++;
+		}
 	}
 	for(;i<length;k++)
 		arr3.Append(A[i++]);
 	for(;j<arr2.length;k++)
 		arr3.Append(arr2.Get(j++));	
+	return arr3;
+}
+
+//this fn works ONLY if input arrays are sorted either BOTH ascendingly 
+//or BOTH descendingly
+Array Array::Union(Array arr2)
+{
+	int i,j,k;
+	i=j=k=0;
+	Array arr3(length+arr2.length);
+	if(arr2.isSorted()!=isSorted()) return arr3;
+	int sorted=arr2.isSorted();
+	if(sorted==1)//ascending
+	{
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]<=arr2.Get(j))
+				arr3.Append(A[i++]);
+			else arr3.Append(arr2.Get(j++));
+			k++;
+		}
+	}
+	else if(sorted==-1)//descending
+	{
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]>=arr2.Get(j))
+				arr3.Append(A[i++]);
+			else arr3.Append(arr2.Get(j++));
+			k++;
+		}
+	}
+	for(;i<length;k++)
+		arr3.Append(A[i++]);
+	for(;j<arr2.length;k++)
+		arr3.Append(arr2.Get(j++));	
+	return arr3;
+}
+
+Array Array::Diff(Array arr2)
+{
+	Array arr3(length);
+	if(arr2.length!=length) return arr3;
+	for(int i=0;i<length;i++)
+		arr3.Append(A[i]-arr2.Get(i));
+	return arr3;
+}
+
+Array Array::Inter(Array arr2)
+{
+	int i,j,k,len=0;
+	i=j=k=0;
+	if(length>=arr2.length)
+		len=length;
+	else len=arr2.length;
+	Array arr3(len);
+	if(isSorted()!=arr2.isSorted()) return arr3;
+	if(isSorted()==1)//asc
+	{
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]<arr2.Get(j)) i++;
+			if(arr2.Get(j)<A[i]) j++;
+			if(A[i]==A[j])
+			{
+				arr3.Insert(k++,A[i]);
+				i++;
+				j++;
+			}
+		}
+	}
+	else if(isSorted()==-1)//des
+	{
+		while(i<length && j<arr2.length)
+		{
+			if(A[i]>arr2.Get(j)) i++;
+			if(arr2.Get(j)>A[i]) j++;
+			if(A[i]==A[j])
+			{
+				arr3.Insert(k++,A[i]);
+				i++;
+				j++;
+			}
+		}
+	}
 	return arr3;
 }
 
